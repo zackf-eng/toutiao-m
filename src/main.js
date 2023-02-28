cbb407540edf2d3144cbd183a4dda30ec8cf222b
zackfeng <zackfeng@163.com>
2023-02-28 18:50:38 +0800

    day01

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 Vant核心组件库
+import Vant from 'vant'
+
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+// 加载全局样式
+import './styles/index.less'
+
+// 加载动态设置REM基准值
+import 'amfe-flexible'
+// 注册使用Vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
