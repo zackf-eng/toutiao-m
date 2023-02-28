b8db0b824023346ab96de4d7c4809c037b06f4f0
zackfeng <zackfeng@163.com>
2023-02-28 20:18:32 +0800

    day02

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
   <div id="app">
-    <h1>黑马头条</h1>
-    <router-view />
-
-    <hr>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-
-    <van-cell-group>
-      <van-cell title="单元格" value="内容" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-    </van-cell-group>
+    <!-- 路由的出口 -->
+    <router-view/>
   </div>
 </template>
 
@@ -23,4 +11,4 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -3,18 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 加载 Vant核心组件库
+// 加载 Vant 核心组件库
 import Vant from 'vant'
 
-// 加载Vant全局样式
+// 加载 Vant 全局样式
 import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.less'
 
-// 加载动态设置REM基准值
+// 加载动态设置 REM 基准值
 import 'amfe-flexible'
-// 注册使用Vant组件库
+
+// 注册使用 Vant 组件库
 Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
